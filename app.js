7369ba70068a606fad85dec68715f170b45b9ac9
Peter <peter12218@gmail.com>
2022-09-21 21:12:49 +0800

    #

--- app.js
+++ app.js
@@ -18,3 +18,21 @@ console.log(friends);
 //unshift
 friends.unshift("Harry");
 console.log(friends);
+
+//Object
+let Peter = {
+    //properties setting
+    firstName: "Peter",
+    lastName: "Hsiao",
+    age: 22,
+    //method setting
+    sayHi() {
+        console.log("Peter says hi");
+    },
+    walk(palce) {
+        console.log("Peter is walking on the " + palce);
+    }
+};
+console.log(Peter.age);
+Peter.sayHi();
+Peter.walk("street");
