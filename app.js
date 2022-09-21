f938c804fab927e7eb8ddb6785a0c591707a88fb
Peter <peter12218@gmail.com>
2022-09-21 20:29:37 +0800

    #

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 let friends = ["John", "Sandy", "Alex"]
 console.log(friends[1]);
-console.log(friends.length)
+console.log(friends.length);
+
+// push
+friends.push("Harry");
+console.log(friends);
+console.log(friends.length);
+
+// pop
+friends.pop();
+console.log(friends);
+
+// shift
+friends.shift();
+console.log(friends);
+
+//unshift
+friends.unshift("Harry");
+console.log(friends);
